f3f1344262690d695734bab2836bdd6e828cfda2
kfmit <kfung@mit.edu>
2021-06-02 13:18:41 -0400

    adding and editing

---
+++ timeseries.m
@@ -0,0 +1,14 @@
+%% 02JUN2021
+% Kat Fung
+% Time Series
+
+clc
+clear
+close all
+
+% One: Import all of the csv from Sigicom2020
+% At least their names, bc you can pull the timestamp from there
+
+% Two: Plot all of these instances of triggering on a timeseries plto
+
+% Three: Plot all the instances WITH temperature data
